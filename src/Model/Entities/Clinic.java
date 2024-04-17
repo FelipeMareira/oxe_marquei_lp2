38c8343216b2051057b907cc81c600bf46392503
Lucas Oliveira <lucassdeoliveira@outlook.com.br>
2024-04-17 08:22:28 -0300

    cria classe clinc e inicia doctor

--- src/Model/Entities/Clinic.java
+++ src/Model/Entities/Clinic.java
@@ -1,4 +1,51 @@
 package Model.Entities;
 
 public class Clinic {
+    int idClinic;
+    String nameOfClinic;
+    String address;
+
+    public Clinic(int idClinic, String nameOfClinic, String address) {
+        this.idClinic = idClinic;
+        this.nameOfClinic = nameOfClinic;
+        this.address = address;
+    }
+
+    public Clinic(String nameOfClinic, String address) {
+        this.nameOfClinic = nameOfClinic;
+        this.address = address;
+    }
+
+    public int getIdClinic() {
+        return idClinic;
+    }
+
+    public void setIdClinic(int idClinic) {
+        this.idClinic = idClinic;
+    }
+
+    public String getNameOfClinic() {
+        return nameOfClinic;
+    }
+
+    public void setNameOfClinic(String nameOfClinic) {
+        this.nameOfClinic = nameOfClinic;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Clinic{" +
+                "idClinic=" + idClinic +
+                ", nameOfClinic='" + nameOfClinic + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
